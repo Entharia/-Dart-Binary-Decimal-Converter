dd9218f9d6300adbf7e2c78d976a762be51b5afd
Entharia <59421695+Entharia@users.noreply.github.com>
2020-08-09 11:03:49 +0200

    Add files via upload

---
+++ bin_dec_converter.dart
@@ -0,0 +1,27 @@
+class Converter {
+  int decimalToBinary(var decimal) {
+    var binary = 0;
+    int i = 1;
+    while (decimal > 0) {
+      binary = binary + (decimal % 2) * i;
+
+      decimal = (decimal / 2).floor();
+
+      i = i * 10;
+    }
+    return binary;
+  }
+
+  int binaryToDecimal(int binary) {
+    int decimal = 0;
+    int lastDigit;
+    int i = 1;
+    while (binary > 0) {
+      lastDigit = binary % 10;
+      binary = (binary / 10).floor();
+      decimal += lastDigit * i;
+      i = i * 2;
+    }
+    return decimal;
+  }
+}
